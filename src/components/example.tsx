207f9b72557db699921c4f7d1580d365e98d02f3
Alton Qian <qian.alton@gmail.com>
2025-10-06 10:27:55 -0700

    added example.tsx

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,14 +1,12 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 bg-cooking-brown-light p-10">
-      <div className="w-1/2 bg-cooking-green p-5 text-left">
-        {props.text1}
-      </div>
+    <div className="bg-cooking-brown-light flex w-3/4 items-center gap-1 p-10">
+      <div className="bg-cooking-green w-1/2 p-5 text-left">{props.text1}</div>
       <div className="flex w-1/2 flex-row gap-1">
-        <div className="w-full bg-cooking-brown-dark p-5 text-left">
+        <div className="bg-cooking-brown-dark w-full p-5 text-left">
           props.text2
         </div>
-        <div className="w-full bg-cooking-gray p-5 text-right">
+        <div className="bg-cooking-gray w-full p-5 text-right">
           {props.text2}
         </div>
       </div>
