b4421dfd4447f3cec5ae60c2f9fab7a7b3d08ac6
Alton Qian <qian.alton@gmail.com>
2025-10-10 11:37:29 -0700

    wow

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -11,7 +11,6 @@ const Example = (props: { text1: string; text2: string; text3: string }) => {
         </div>
       </div>
       {/* Hint: Add another box/div here similarly to line 4 */}
-      <div>Hello</div>
     </div>
   );
 };
