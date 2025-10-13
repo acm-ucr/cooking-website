2c97ea6648497adb4d890df9b8f55282ada5a452
eltont45 <eltont45@gmail.com>
2025-10-12 21:36:14 -0700

    added flexbox practice code to example.tsx and page.tsx

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,16 +1,25 @@
-const Example = (props: { text1: string; text2: string; text3: string }) => {
+const Example = (props: {
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+}) => {
   return (
-    <div className="bg-cooking-brown-light flex w-3/4 items-center gap-1 p-10">
-      <div className="bg-cooking-green w-1/2 p-5 text-left">{props.text1}</div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-cooking-brown-dark w-full p-5 text-left">
-          props.text2
-        </div>
-        <div className="bg-cooking-gray w-full p-5 text-right">
+    <div className="bg-cooking-brown-dark w-2/3 flex-col gap-1 rounded-lg p-10">
+      <div className="bg-cooking-green m-auto mb-12 w-1/2 p-5 text-center text-white">
+        {props.text1}
+      </div>
+      <div className="m-auto flex w-1/2 flex-row gap-1">
+        <div className="bg-cooking-brown-light w-full rounded-full p-5 text-center text-white">
           {props.text2}
         </div>
+        <div className="bg-cooking-cream-light w-full rounded-full p-5 text-center text-white">
+          {props.text3}
+        </div>
       </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
+      <div className="bg-cooking-gray m-auto mt-12 w-1/2 p-5 text-center text-white">
+        {props.text4}
+      </div>
     </div>
   );
 };
