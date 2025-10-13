05779e9d9116174b8f46d3af767e9ccc848a8c21
Nimrah S <nimrahs@Nimrahs-MacBook-Pro.local>
2025-10-12 21:02:29 -0700

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,21 @@
+import { Icon } from "@iconify/react";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex h-1/8 w-screen flex-col items-center justify-center bg-gray-100">
+      <div className="flex h-full w-full flex-row items-center gap-6">
+        <div className="flex h-1/64 w-full bg-black"></div>
+        <Icon icon="ph:linktree-logo-bold" width="64" height="64" />
+        <Icon icon="mdi:instagram" width="64" height="64" />
+        <Icon icon="ic:baseline-discord" width="64" height="64" />
+        <div className="flex h-1/64 w-full bg-black"></div>
+      </div>
+
+      <div className="font-cooking-main text-cooking-gray mt-1/16 flex h-full w-full justify-center font-extralight">
+        cooking club @ ucr
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
