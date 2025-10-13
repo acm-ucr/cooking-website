93fe4efe6a224e0fc3c081ef21b56619e82bba1e
EveningSt3r <daawesomeboi@gmail.com>
2025-10-12 23:57:07 -0700

    PR #2, fixed gap issues and example.tsx, changes by Samuel You

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -5,22 +5,20 @@ const Example = (props: {
   text4: string;
 }) => {
   return (
-    <div className="flex w-full justify-center">
-      <div className="bg-cooking-brown-dark flex w-3/4 flex-col items-center gap-6 rounded-xl p-10">
-        <div className="bg-cooking-green w-1/4 rounded-md p-5 text-center text-white">
-          {props.text1}
+    <div className="bg-cooking-brown-dark w-2/3 flex-col gap-1 rounded-lg p-10">
+      <div className="bg-cooking-green m-auto mb-12 w-1/2 p-5 text-center text-white">
+        {props.text1}
+      </div>
+      <div className="m-auto flex w-1/2 flex-row gap-1">
+        <div className="bg-cooking-brown-light w-full rounded-full p-5 text-center text-white">
+          {props.text2}
         </div>
-        <div className="between my-2 flex w-1/4 flex-row justify-center gap-4">
-          <div className="bg-cooking-brown-light flex-1 rounded-full px-8 py-4 text-center text-white">
-            {props.text2}
-          </div>
-          <div className="bg-cooking-cream-light flex-1 rounded-full px-8 py-4 text-center text-white">
-            {props.text3}
-          </div>
+        <div className="bg-cooking-cream-light w-full rounded-full p-5 text-center text-white">
+          {props.text3}
         </div>
-        <div className="bg-cooking-gray w-1/4 rounded-md p-5 text-center text-white">
-          {props.text4}
-        </div>
+      </div>
+      <div className="bg-cooking-gray m-auto mt-12 w-1/2 p-5 text-center text-white">
+        {props.text4}
       </div>
     </div>
   );
