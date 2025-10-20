73bbfe61e470a678b0a289f1326b35be3879c60e
Nimrah S <nimrahs@ucr-secure-43-10-13-220-245.wnet.ucr.edu>
2025-10-20 09:37:04 -0700

    fixing build error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ const Footer = () => {
     <div className="flex h-1/8 w-screen flex-col items-center justify-center bg-gray-100">
       <div className="flex h-full w-full flex-row items-center gap-6">
         <div className="flex h-1/64 w-full bg-black"></div>
-        <Icon icon="ph:linktree-logo-bold" width="64" height="64" />
+        {/* <Icon icon="ph:linktree-logo-bold" width="64" height="64" />
         <Icon icon="mdi:instagram" width="64" height="64" />
-        <Icon icon="ic:baseline-discord" width="64" height="64" />
+        <Icon icon="ic:baseline-discord" width="64" height="64" /> */}
         <div className="flex h-1/64 w-full bg-black"></div>
       </div>
 
