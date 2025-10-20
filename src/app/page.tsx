8504ff7587640b94b61e5bb831f8a0a9a0ff25f3
Alton Qian <qian.alton@gmail.com>
2025-10-20 14:25:28 -0700

    test merge conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Example text1="Adjust" text2="These" text3="Props" />
+      <div>wefojweiofj</div>
     </div>
   );
 };
